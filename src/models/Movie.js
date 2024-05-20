fcdae6b5f60c1343eb2a4db5452655572ed1e067
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-20 16:57:49 +0530

    feat: create movie model

---
+++ src/models/Movie.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String
+  },
+  publishingYear: {
+    type: String
+  },
+  imageUrl: {
+    type: String
+  }
+})
+
+const Movie = mongoose.model("Movie",movieSchema)
+
+module.exports = Movie
