fd2579eb7f04962df24fbb479b626ff17b195152
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-21 16:59:34 +0530

    feat: add swagger config

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
 const userRoutes = require("./src/routes/user.routes");
 const movieRoutes = require("./src/routes/movie.routes");
 const authenticateToken = require("./src/middlewares/auth.middleware");
 const app = express();
 
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "src", "docs", "swagger-bundle.yaml")
+); //"./src/docs/swagger.yml";
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/user/", userRoutes);
 app.use("/api/movie/", authenticateToken, movieRoutes);
